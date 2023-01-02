8f09abc279de832d88581ea2eb45c699982cdb81
Narendhiran13 <narendhiran2000@gmail.com>
2023-01-02 10:00:33 -0700

    Nodes, Publisher and Subscribers
    Created multiple nodes for the robot in side a package.
    Create a message topic through topic_name.
    Created an service to request for camera image from a camera_topic.
    Subscribed to an node and automated the model to reach a position to view an cubes.

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/thomasfriends/Documents/GitHub/HomeServiceRobot/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/thomasfriends/Documents/GitHub/HomeServiceRobot/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/thomasfriends/Documents/GitHub/HomeServiceRobot/catkin_ws/devel;/opt/ros/noetic' != '' else []
