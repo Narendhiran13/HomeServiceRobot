4a2f5ba4f0c9db55e2c0619264845e80f76ca33e
Narendhiran13 <narendhiran2000@gmail.com>
2023-01-07 02:16:48 -0700

    New workspace built from scratch
    Created a new catkin workspace
    Integrated new world, and URDF robot.
    Added plugin to interact with the robot

---
+++ catkin_wsP2/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/thomasfriends/Documents/GitHub/HomeServiceRobot/catkin_wsP2/src
+# Build directory: /home/thomasfriends/Documents/GitHub/HomeServiceRobot/catkin_wsP2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
---
+++ catkin_wsP2/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/thomasfriends/Documents/GitHub/HomeServiceRobot/catkin_wsP2/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/thomasfriends/Documents/GitHub/HomeServiceRobot/catkin_wsP2/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/thomasfriends/Documents/GitHub/HomeServiceRobot/catkin_wsP2/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/thomasfriends/Documents/GitHub/HomeServiceRobot/catkin_wsP2/src:$ROS_PACKAGE_PATH"
